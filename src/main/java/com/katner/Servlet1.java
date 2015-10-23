553790207429420d625cc1aa28fa436033e62f58
Michal Katner <katnerster@gmail.com>
2015-10-23 23:16:56 +0200

    Initial commit

---
+++ src/main/java/com/katner/Servlet1.java
@@ -0,0 +1,25 @@
+package com.katner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by michal on 23.10.15.
+ */
+@WebServlet(name = "Servlet1")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        writer.write("Witam pana");
+    }
+}
